0ac104a4d580f7142448a892d65bfd629d285519
Valentin Immler-Valchev <vvvalvalchev@gmai.com>
2021-03-03 13:07:44 +0100

    exc.Basic-Advanced with comments

--- script.js
+++ script.js
@@ -13,9 +13,34 @@ let players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
 console.log("The most valuable player of the match is " + players[2]);
 
 // Intermediate Exercise 1 
-
+// Using the sort method to sort all the elements in the array alphabetically 
 let cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
 
 cars.sort();
 
 console.log(cars);
+
+// Intermediate Exercise 2
+
+let fruits = [ "apple", "banana", "kiwi"];
+
+// // Using the push method
+fruits.push("orange");
+console.log(fruits);
+
+// Using the pop method
+fruits.pop("orange");
+console.log(fruits);
+
+
+let animals = ["monkey", "horse", "dog"];
+
+// Using the sort method for an alphabetical order
+animals.sort();
+console.log(animals);
+
+//Using the unshift method to add a "cat" in the beginning of the array 
+animals.unshift("cat");
+console.log(animals);
+
+// Intermediate Excercise 3
