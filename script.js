a916aa1cfb6aff7ed78eda98e822ff320b79156b
Matthias Mauritz <Matthias.mauritz1@gmail.com>
2021-03-03 10:03:40 +0100

    added the skeleton files

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Basic Exercise 1 */
+
+console.log("Hello from the Console");
+document.write("Hello from the Browser");
+
+// Basic Exercise 2 
+
+let age = 25;
+console.log("Hi my name is Martin and I am " + age + " years old");
+
+let players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+
+console.log("The most valuable player of the match is " + players[2]);
+
+// Intermediate Exercise 1 
+
+let cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
+
+cars.sort();
+
+console.log(cars);
